d8708345354147305997f47a452a2008bddac3ed
Karin Lin <klin@basistech.com>
2021-06-18 17:02:23 -0400

    demo app for antd Menu bug

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
+import { Menu } from 'antd';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+	    <img src={logo} width="150" style={{ padding: 0, margin: '0 25px 0 0' }} alt="logo" />
+        <Menu.Item key="1" >
+          <span>Item1</span>
+        </Menu.Item>
+        <Menu.Item key="2">
+          <span>Item2</span>
+        </Menu.Item>
+        <Menu.Item key="3">
+          <span>Item3</span>
+        </Menu.Item>
+      </Menu>
   );
 }
 
